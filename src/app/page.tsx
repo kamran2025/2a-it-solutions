08e8ad45868fb4ae96eed8260dc2ae16e9413262
kamran2025 <144653562+kamran2025@users.noreply.github.com>
2024-03-28 01:53:12 +0530

    github glob add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import FeaturedProducts from "@/components/FeaturedProducts";
+import { GlobeDemo } from "@/components/GithubGlob";
 import HeroSection from "@/components/HeroSection";
+import { Globe } from "@/components/ui/globe";
 
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/95 antialiased bg-grid-white/[0.02]">
-      <HeroSection/>
-      <FeaturedProducts/>
+      <HeroSection />
+      <FeaturedProducts />
+      <GlobeDemo />
     </main>
   );
 }
