0d580bf1bc4e17e8a5441a946b5c8e496f5e838d
kamran2025 <144653562+kamran2025@users.noreply.github.com>
2024-03-29 13:58:14 +0530

    remove featured product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import FeaturedProducts from "@/components/FeaturedProducts";
 import {HeroSection} from "@/components/HeroSection";
 
 
@@ -6,7 +5,6 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/95 antialiased bg-grid-white/[0.02]">
       <HeroSection />
-      <FeaturedProducts />
       
     </main>
   );
