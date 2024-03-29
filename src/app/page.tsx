617e9fe69d91465068464c7dc578a27fad23ad6d
kamran2025 <144653562+kamran2025@users.noreply.github.com>
2024-03-30 03:12:37 +0530

    add pt in home connect life section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,11 +26,11 @@ export default function Home() {
         <h1 className={cn("w-full text-center md:text-4xl text-3xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-400  font-sans font-bold absolute top-10 z-20")}>
           Connect Life Solutions
         </h1>
-        <div className="z-40 opacity-80">
+        <div className="z-40 opacity-85">
           <img src={connectImg.src} className=" md:max-w-xl" alt="image" />
         </div>
         <div>
-          <p className="text-neutral-300 text-xl max-w-2xl relative z-20">
+          <p className="text-neutral-300 text-xl max-w-2xl pt-6 lg:pt-0 relative z-20">
             ConnectLifeSolutions is in the business of IT consulting and Enterprise Software services to customers in India, North America, UK and Australia. At Connect Life, we are committed to customer success through transparent communication and consistent quality services.
           </p>
           <p className="mt-8 text-neutral-300 text-xl max-w-2xl relative z-20">
@@ -38,6 +38,10 @@ export default function Home() {
           </p>
         </div>
       </div>
+
+      {/* Vision and Mission  */}
+
+
 
     </main>
   );
